eabdfd035d499bae73f89d0ba9f78ebe16967108
Jannik Bamberger <dev.jbamberger@gmail.com>
2020-05-17 11:14:26 +0200

    implement effect based updates

--- rgb_stripe/rgb_stripe.cpp
+++ rgb_stripe/rgb_stripe.cpp
@@ -1,25 +1,49 @@
 #include "rgb_stripe.h"
 #include "Arduino.h"
 
-RgbController::RgbController(int r_pin, int g_pin, int b_pin, bool high_is_on)
-    : _r_pin(r_pin), _g_pin(g_pin), _b_pin(b_pin), _high_is_on(high_is_on) 
+RgbController::RgbController(int r_pin, int g_pin, int b_pin, ColorEffect *effect, bool high_is_on)
+    : _r_pin(r_pin), _g_pin(g_pin), _b_pin(b_pin),
+      _high_is_on(high_is_on), _last_update(millis()), _effect(effect)
 {
     pinMode(_r_pin, OUTPUT);
     pinMode(_g_pin, OUTPUT);
     pinMode(_b_pin, OUTPUT);
 
-    set_color({0,0,0});
+    set_color({0, 0, 0});
 }
 
 void RgbController::set_color(Color color)
 {
-    if (_high_is_on) {
+    if (_high_is_on)
+    {
         analogWrite(_r_pin, color.r);
         analogWrite(_g_pin, color.g);
         analogWrite(_b_pin, color.b);
-    } else {
+    }
+    else
+    {
         analogWrite(_r_pin, 255 - color.r);
         analogWrite(_g_pin, 255 - color.g);
         analogWrite(_b_pin, 255 - color.b);
     }
+}
+
+void RgbController::set_effect(ColorEffect *effect)
+{
+    this->_effect = effect;
+}
+
+void RgbController::update()
+{
+    unsigned long time = millis();
+    unsigned long delta = time - _last_update;
+    _last_update = time;
+    _effect->update(*this, delta);
+}
+
+ConstantColorEffect::ConstantColorEffect(Color color) : _color(color) {}
+
+void ConstantColorEffect::update(RgbController &controller, unsigned long delta)
+{
+    controller.set_color(this->_color);
 }
--- rgb_stripe/rgb_stripe.h
+++ rgb_stripe/rgb_stripe.h
@@ -1,27 +1,48 @@
 #ifndef rgb_stripe
 #define rgb_stripe
 
-
 struct Color
 {
     int r;
     int g;
     int b;
-    Color(int r, int g, int b): r(r), g(g), b(b) {}
+    Color(int r, int g, int b) : r(r), g(g), b(b) {}
+};
+#include "colors.h"
+
+class RgbController;
+class ColorEffect
+{
+
+public:
+    virtual void update(RgbController &controller, unsigned long delta);
 };
 
 class RgbController
 {
+    unsigned long _last_update;
     bool _high_is_on;
     int _r_pin;
     int _g_pin;
     int _b_pin;
+    ColorEffect *_effect;
 
 public:
-    explicit RgbController(int r_pin, int g_pin, int b_pin, bool high_is_on = true);
+    explicit RgbController(
+        int r_pin, int g_pin, int b_pin,
+        ColorEffect *effect, bool high_is_on = true);
     void set_color(Color color);
+    void set_effect(ColorEffect *effect);
+    void update();
 };
 
-#include "colors.h"
+class ConstantColorEffect : public ColorEffect
+{
+    Color _color;
+
+public:
+    ConstantColorEffect(Color color);
+    virtual void update(RgbController &controller, unsigned long delta);
+};
 
 #endif rgb_stripe
