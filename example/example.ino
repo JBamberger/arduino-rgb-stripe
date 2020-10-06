c0cd831d2406fa1c3a1a1561bd523a1166e3a413
Jannik Bamberger <dev.jbamberger@gmail.com>
2020-10-06 22:26:00 +0200

    create example sketch

--- example/example.ino
+++ example/example.ino
@@ -1,1 +1,37 @@
-// TODO:
+#include <colors.h>
+#include <rgb_stripe.h>
+
+const int RED_PIN = 3;
+const int GREEN_PIN = 4;
+const int BLUE_PIN = 5;
+
+ConstantColorEffect r_effect(RED);
+ConstantColorEffect g_effect(GREEN);
+ConstantColorEffect b_effect(BLUE);
+
+RgbController controller(RED_PIN, GREEN_PIN, BLUE_PIN, &r_effect, false);
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("1=Red, 2=Green, 3=Blue");
+}
+
+void loop() {
+  if (Serial.available()) {
+    int input = Serial.read();
+    switch (input) {
+      case '1': controller.set_effect(&r_effect); break;
+      case '2': controller.set_effect(&g_effect); break;
+      case '3': controller.set_effect(&b_effect); break;
+      case '\r':
+      case '\n':
+        break;
+      default:
+        Serial.println("Invalid input.");
+        Serial.println(input);
+        break;
+    }
+  }
+  controller.update();
+}
